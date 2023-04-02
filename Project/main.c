58e7569c434ed245571884200919135365fea400
PedroVMota <pedromvmota@outlook.pt>
2023-04-02 21:42:20 +0100

    Final Update

---
+++ Project/main.c
@@ -0,0 +1,19 @@
+#include "./include/ft_printf.h"
+#include "./include/ft_colors.h"
+#include <limits.h>
+
+int main(void)
+{
+    system("clear");
+    sleep(1);
+
+    int return_val;
+    
+    return_val = printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+    printf("\nReturn Value: %i\n", return_val);
+
+    return_val = 0;
+    return_val = ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+    printf("\nReturn Value: %i\n", return_val);
+    return 0;
+}
